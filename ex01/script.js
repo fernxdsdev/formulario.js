6c77305ce169330edc496ae8f8b251583d8b0ffb
Bryan Fernandes <bryancmfm12@gmail.com>
2026-06-25 21:21:18 -0300

    Adicionando mensagens de validaçao

---
+++ ex01/script.js
@@ -0,0 +1,34 @@
+// Capturar o formulário 
+
+const formEx01 = document.getElementById("formEx01");
+
+formEx01.addEventListener("submit", function (evento){
+
+    evento.preventDefault();
+
+    //capturar inputs do HTML
+
+    const nome = document.getElementById("nome");
+    const cpf = document.getElementById("cpf");
+
+    //Testar nome
+    if(nome.value.trim() == ""){ 
+        //Invalido
+        nome.classList.add("is-invalid")
+        nome.classList.remove("is-valid")
+    } else{
+        //Valido
+        nome.classList.add("is-valid");
+        nome.classList.remove("is-invalid");
+    }
+    if(cpf.value.trim() ==""){
+        cpf.classList.add("is-invalid");
+        cpf.classList.remove("is-valid");
+        } else{
+            cpf.classList.add("is-valid");
+            cpf.classList.remove("is-invalid");
+        }
+
+    
+
+});
